970292d324e9e5707f0f4cae513533a5eddce9a6
lrscy <ruosenlee@gmail.com>
2016-09-30 17:03:29 +0800

    16青岛网赛 07 补题

--- 组队训练/2016-09-17 Qingdao Online/1007.cpp
+++ 组队训练/2016-09-17 Qingdao Online/1007.cpp
@@ -3,25 +3,32 @@ using namespace std;
 typedef long long LL;
 const int MAXN = 1e5 + 10;
 
-int a[MAXN];
+LL a[MAXN];
 int n, m;
 
 LL gao( int mid ) {
+    queue<LL> q, qs;
     LL ret = 0;
-    priority_queue<int, vector<int>, greater<int> > pq;
-    for( int i = 0; i < n; ++i ) pq.push( a[i] );
     int tmp = ( n - 1 ) % ( mid - 1 );
-    for( int i = 0; i < tmp; ++i ) pq.push( 0 );
-    while( pq.size() > 1 ) {
-        LL tmp = 0;
-        for( int i = 0; i < mid && !pq.empty(); ++i ) {
-            tmp += pq.top();
-            pq.pop();
+    if( tmp ) for( int i = 0; i < mid - 1 - tmp; ++i ) q.push( 0 );
+    for( int i = 0; i < n; ++i ) q.push( a[i] );
+    while( !q.empty() || !qs.empty() ) {
+        LL sum = 0;
+        for( int i = 0; i < mid; ++i ) {
+            if( q.empty() && qs.empty() ) break;
+            if( q.empty() ) { sum += qs.front(); qs.pop(); }
+            else if( qs.empty() ) { sum += q.front(); q.pop(); }
+            else {
+                int u = q.front(), v = qs.front();
+                if( u < v ) { sum += u; q.pop(); }
+                else { sum += v; qs.pop(); }
+            }
         }
-        ret += tmp;
-        pq.push( tmp );
+        ret += sum;
+        if( q.empty() && qs.empty() ) break;
+        qs.push( sum );
     }
-    return ret;
+    return ret <= m;
 }
 
 int main() {
@@ -29,11 +36,12 @@ int main() {
     scanf( "%d", &t );
     while( t-- ) {
         scanf( "%d%d", &n, &m );
-        for( int i = 0; i < n; ++i ) scanf( "%d", a + i );
-        int low = 1, high = n;
+        for( int i = 0; i < n; ++i ) scanf( "%I64d", a + i );
+        sort( a, a + n );
+        int low = 2, high = n;
         while( low < high ) {
             int mid = ( low + high ) >> 1;
-            if( gao( mid ) <= m ) high = mid;
+            if( gao( mid ) ) high = mid;
             else low = mid + 1;
         }
         printf( "%d\n", high );
